0797382ad2402959a35b33987964980297e0e1a9
Nicolas Colorado <nicolasaug.colorado@gmail.com>
2022-02-15 22:27:54 -0500

    P0 Updated Database Script and Diagram

--- ShopDatabaseScript.sql
+++ ShopDatabaseScript.sql
@@ -16,7 +16,6 @@ create table Orders
 	TotalPrice float
 )
 
-
 create table Product
 (
 	productID int identity(1,1) primary key,
@@ -28,16 +27,27 @@ create table Product
 
 insert Product (Name, Price, Category, Quantity)
 values ('Men''s Goldlink Chain', 290.00, 'Jewelery', 10),
+	   ('Elsa PerettiDiamond Bracelet', 575.00, 'Jewelry', 3),
+	   ('Sterling Silver Mood Ring', 32.95, 'Jewelry', 40),
 	   ('Rogue RA-090 Dreadnought Acoustic Guitar', 89.99, 'Electronics and Musical Instruments', 4),
-	   ('Mitchell MS450 Modern Single-Cutaway Electric Guitar', 399.99, 'Electronics and Musical Instruments', 4),
-	   ('Pokemon: Trading Card Game Rayquaza or Noivern V Battle Deck', 14.99,  'Toys and Games', 20),
+	   ('Mitchell MS450 Single-Cutaway Electric Guitar', 399.99, 'Electronics and Musical Instruments', 4),
+	   ('Insignia CD Boombox', 39.99, 'Electronics and Musical Instruments', 12),
+	   ('Samsung USB-C Data Charging Cable', 6.99, 'Electronics and Musical Instruments', 30),
+	   ('Pokemon: Trading Card Game Rayquaza Battle Deck', 14.99,  'Toys and Games', 20),
 	   ('Sony PlayStation 3 500 GB System', 217.99, 'Toys and Games', 5),
-	   (6, )
-
-
-alter table Product 
-add Quantity int
-
+	   ('Nerf Rival Hypnos XIX-1200', 52.99, 'Toys and Games', 6),
+	   ('Belgian Chocolate Cake', 24.99, 'Produce and Baked Goods', 5),
+	   ('Golden Apple', 1.50, 'Produce and Baked Goods', 50),
+	   ('Chiquita Bananas 2-lb', 4.39, 'Produce and Baked Goods', 25),
+	   ('Krispy Kreme Original Glazed Dozen Box', 7.99, 'Produce and Baked Goods', 9),
+	   ('Women''s Floral Dress', 39.99, 'Clothing', 10),
+	   ('Men''s Cotton Sweatpants', 27.96, 'Clothing', 8),
+	   ('Children''s One-Piece Pajama Set', 18.50, 'Clothing', 11),
+	   ('Everlast MMA Gloves', 34.99, 'Sports Equipment', 2),
+	   ('Adidas MLS Club Soccer Ball', 19.99, 'Sports Equipment', 7),
+	   ('CAP Barbell Neoprene 16-lb Dumbbell', 16.99, 'Sports Equipment', 14)
+	   
+	   
 create table StoreFront 
 (
 	storeID int identity(1,1) primary key,
@@ -69,6 +79,17 @@ select * from StoreFront sf
 inner join Orders o on sf.storeID = o.storeID
 
 
+---Error fixing after adding Products
+drop table LineItems 
+
+drop table Inventory 
+
+drop table Product 
+
+
+
+
+
 
 --(Many-to-Many Relationships)
 -- Orders + Products = Lineitems
@@ -89,10 +110,11 @@ create table Inventory
 (
 	storeID int foreign key references Storefront(storeID),
 	productID int foreign key references Product(productID),
+	Quantity int
 )
 
-alter table Inventory 
-add Quantity int
+--alter table Inventory 
+--add Quantity int
 
 select p.productID, p.Name, p.Price, p.Category, i.Quantity from StoreFront sf 
 inner join Inventory i on sf.storeID = i.storeID
